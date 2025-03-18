231df11ae776c4f7b86388cbdad3b95f72ef1ec5
Gurusamy-zoho <gurusamysivasankar591@gmail.com>
2025-03-18 21:13:46 +0530

    feat: create users table

--- UserandTasks.sql
+++ UserandTasks.sql
@@ -1,1 +1,13 @@
 # Write Two table one is Users and another table is tasks for todo-app
+
+CREATE DATABASE TodoList;
+use TodoList;
+
+CREATE TABLE Users (
+    User_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    Password VARCHAR(255) NOT NULL
+);
+
+SELECT * from users
