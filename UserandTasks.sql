5a0474498368f73457e22c7c939f9a16d69d0b24
Gurusamy-zoho <gurusamysivasankar591@gmail.com>
2025-03-19 13:04:17 +0530

    feat: create tasks table

--- UserandTasks.sql
+++ UserandTasks.sql
@@ -11,3 +11,11 @@ CREATE TABLE Users (
 );
 
 SELECT * from users
+
+CREATE TABLE Tasks (
+    TaskID INT PRIMARY KEY AUTO_INCREMENT,
+    TaskName VARCHAR(255) NOT NULL,
+    IsCompleted ENUM('true', 'false') DEFAULT 'false',
+    UserEmail VARCHAR(255),
+    CONSTRAINT fk_user_email FOREIGN KEY (UserEmail) REFERENCES Users(Email)
+);
